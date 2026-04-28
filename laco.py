8edb08a9e8c1ddb53bfa2424c8a0722e0ef657ae
pdzinxzt <vieirapedrohenrique642@gmail.com>
2026-04-28 01:15:17 +0000

    index

--- laco.py
+++ laco.py
@@ -1,7 +1,15 @@
-sinal = "vermelho"
+# exemplo de codigo para controlar o cenario
+sinal =input
 
+# input ("Digite a cor do Sinal:")
+sinal = input("Digite a cor do Sinal: ")
+
+# if e elif para verificar a cor do sinal e imprimir a mensagem correspondente
 if sinal == "vermelho":
-    print("Pare")
- elif sinal == "amarelo":
-    print("ATENCÃO!")
-    else
+    print("Pare!!!!")
+elif sinal == "amarelo":
+    print("ATENCÃO!") 
+elif sinal == "verde":
+    print("Pode Passar!!!!")
+else:
+    print("Cor Invalida")
---
+++ sistema_de_usuario.py
@@ -0,0 +1,7 @@
+usuario = input("login: ")
+senha = input("Senha: ")
+
+if usuario == "fudencio" and senha == "123":
+    print("Acesso Liberado")
+else:
+    print("Acesso Negado")
