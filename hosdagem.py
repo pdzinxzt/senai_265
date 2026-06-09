e303aac2e2f556ccc2e58daa98fdeb133262fc8b
pdzinxzt <vieirapedrohenrique642@gmail.com>
2026-06-08 22:06:02 -0300

    hospedagem

--- hosdagem.py
+++ hosdagem.py
@@ -2,5 +2,5 @@ def formatar_real(valor):
     return f"R$ {valor:,.2f}".replace(",", "X").replace(".", ",").replace("X", ".")
 
 # Uso:
-preco_hospedagem = 49.9
+preco_hospedagem = float(input("Digite o valor da Hospedagem"))
 print(formatar_real(preco_hospedagem))  # Saída: R$ 49,90
